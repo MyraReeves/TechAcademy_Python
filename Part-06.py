36c550d5690d2a7a98f36f931873d3a63566447d
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-25 02:13:32 -0700

    Shows what was learned in the video lesson

---
+++ Part-06.py
@@ -0,0 +1,25 @@
+myList = [2, 3, 4]
+
+# Display how many values are inside the array:
+print(len(myList))
+
+# Use a for loop to iterate thru and display each value of each index inside of the array:
+for i in myList: print(i)
+
+# Show the value of the third item:
+print(myList[2])
+
+# Show the value of the first:
+print(myList[0])
+
+# Add the number 5 onto the end of the array:
+myList.append(5)
+
+# Loop thru and display each of the values NOW in the new array:
+for i in myList: print(i)
+
+# Confirm the length of the new array:
+len(myList)
+
+# To show that the list is an array:
+print(myList)
