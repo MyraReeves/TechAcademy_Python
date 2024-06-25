18212a9a7e8e81b6cbd74609b4b0c675313bfaa2
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-25 15:26:32 -0700

    Fulfills all challenges in assignment

---
+++ Part-08-TuplesChallenges.py
@@ -0,0 +1,10 @@
+# 1. Create a tuple and print it:
+cherryTreeTuple = ("Japanese", "sour", "black", "wild")
+print(cherryTreeTuple)
+
+# 2. Loop through the tuple items by using a for loop:
+for c in cherryTreeTuple: print(c)
+
+# 3. Use the count() mehtod on the tuple:
+numberOfJapaneseCherryTrees = cherryTreeTuple.count("Japanese")
+print(numberOfJapaneseCherryTrees)
