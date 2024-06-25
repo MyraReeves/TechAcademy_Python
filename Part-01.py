889558dd41bf00347c0279de903e7cb682172fb0
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-24 23:24:18 -0700

    Following along with the contents of the first few videos

---
+++ Part-01.py
@@ -0,0 +1,43 @@
+print("Hello, World!")
+
+number1 = 4
+number1
+type(number1)
+
+number2 = 5
+number2
+type(number2)
+
+number3 = number1 + number2
+number3
+
+
+num1 = 1
+num2 = 2
+print(num1 + num2)
+
+num1 = 1.2
+num2 = 2.1
+print(num1 + num2)
+type(num1 + num2)
+
+greeting = "Hello there!"
+greeting
+type(greeting)
+
+# Length (ie how many characters long) is asked for using:
+len(greeting)
+
+# Numbering begins at zero
+greeting[0]
+greeting[1]
+
+
+fName = "Daniel"
+lname = "Christie"
+print(fName + lname)
+
+print(fName + " " + lname)
+
+print("Hi there, {} {}!  Welcome to writing Python!".format(fName, lname) )
+
