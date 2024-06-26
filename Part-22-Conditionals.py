27e23c3cf9998db108cb093e40295f218e09d1b0
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-26 01:03:18 -0700

    Shows content from video lesson

---
+++ Part-22-Conditionals.py
@@ -0,0 +1,12 @@
+
+num1 = 12
+key = True
+
+
+if num1 == 12:
+    if key: 
+        print("Yes! Num1 equals 12 and you have the key!")
+    else:
+        print("Well, Num1 indeed equals 12, but you don't have the key.")
+else:
+    print("Oh noes!  Soemthing went wrong!")
