2dcfd46c496f7c15d9096a61eb01919addde5a36
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-25 20:19:11 -0700

    Fulfills assignment

---
+++ part-13-Array.py
@@ -0,0 +1,6 @@
+# An array is a collection of similar values systematically stored within a collection object.
+# Python treats collection objects such as liss, tuples, and dictionaries as arrays.
+
+color = ['red', 'blue', 'green', 'orange', 'pink', 'purple', 'yellow', 'black', 'brown']
+
+print(color)
