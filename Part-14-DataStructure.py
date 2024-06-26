014f2d0b64a4276b33aae273d48a86fd02943a34
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-25 20:42:06 -0700

    Shows the contents of the video lesson

---
+++ Part-14-DataStructure.py
@@ -0,0 +1,21 @@
+# A data structure is a collection of data values arranged in a special orientation where the data can be quickly accessed from the computer's memory and applied.
+
+"This is a string"
+
+variable = "Bob"
+
+print(variable)
+
+integer = 9
+
+a_float = 9.01
+
+a_list = ["Bob", "Sally"]
+
+a_tuple = (1,2,3,"a","b","c")
+
+a_dictionary = {"index1":"value1" , "index2":"value2"}
+
+Boolean = True
+
+print(type(variable))
