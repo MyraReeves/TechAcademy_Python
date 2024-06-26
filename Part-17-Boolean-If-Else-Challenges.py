0165deab96f7bb30b9cd8992d56456f929cbd6de
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-25 22:51:55 -0700

    Fulfills challenge assignment

---
+++ Part-17-Boolean-If-Else-Challenges.py
@@ -0,0 +1,10 @@
+a = 500
+b = 30
+
+# 1. Run an if/else statement that prints confirmation when the condition is True:
+if a > b: print(str(a) + " is greater than " + str(b))
+else: print("Attempt failed")
+
+# 2. Run an if/else statement that prints when the condition is False:
+if a == b: print(str(a) + " is equal to " + str(b))
+else: print("SUCCESS!!! " + str(a) + " does NOT equal " + str(b) )
