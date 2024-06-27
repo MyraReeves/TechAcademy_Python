7a1a16f84f416b9c7d9f4fb20aac921f14349794
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-27 00:19:47 -0700

    Fulfills assignment

---
+++ Part-40-Variables.py
@@ -0,0 +1,13 @@
+def getSum(number1, number2):
+    answer = number1 + number2
+    print("The sum is {}".format(answer))
+
+
+# Variables can be used to store functions.
+additionVariable = getSum
+
+additionVariable(2, 4)
+
+
+# Variables can also be used to store entire dictionaries of information.
+# Variables can also be used to store an extensive list of items.
