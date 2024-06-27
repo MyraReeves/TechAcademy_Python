e6047e70ec3e5b4f902a4524dbc2324c3bc9760d
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-26 21:02:38 -0700

    Shows content of lesson

---
+++ Part-28-ArrayLoop.py
@@ -0,0 +1,7 @@
+name = 'Python'
+
+# Count the total number of characters within the string:
+print(len(name))
+
+# Use the built-in enumerate method to iterate through all of the elements within the string:
+for i in enumerate(name): print(i)
