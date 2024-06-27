2368e8f07df366b5dbb9199998d3aa57744729fe
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-26 22:09:26 -0700

    Shows lesson content from lecture video

--- Part-29-Functions.py
+++ Part-29-Functions.py
@@ -1,17 +1,27 @@
 
 # Create a variable:
-mySentence = "I like the color"
+mySentence = "loves the color"
 
 # Create a list:
 colorList = ['red', 'white', 'blue', 'pink', 'purple', 'black', 'green']
 
 # Create a function to show a sentence for each color
-def color_function():
+def color_function(nameParameter):
+    # Create a temporary list and leave it empty:
+    tempList = []
     # Iterate thru the color list:
     for i in colorList:
-        # Insert two wild cards and format those strings by plugging in the mySentence variable followed by the iteration:
-        print("{} {}".format(mySentence, i))
+        # Use a variable named "message" and insert three wild cards and format those strings by plugging in the nameParameter, then the mySentence variable, and lastly followed by the iteration:
+        message = "{} {} {}".format(nameParameter, mySentence, i)
+        # Append each iteration's result into the end of the list to form an array:
+        tempList.append(message)
+    # After the entire list of colors is finished, end the loop with a "return" command to show the array of resulting sentences:
+    return tempList
     
 
-# Call the function
-color_function()
+# Call the function with the desired argument name and save the result inside a variable
+colorSentences = color_function("Sally")
+
+# Loop thru the array of sentences to print each to the screen. This step is needed so that the result doesn't look like an array.  It is needed so that instead we see only the resulting sentences each one after another.
+for i in colorSentences:
+    print(i)
