91464047cb6e1dbe38b341b78ea585bd56fafb96
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-26 23:31:57 -0700

    Shows content of lesson

---
+++ Part-35-WildcardPlaceholders.py
@@ -0,0 +1,6 @@
+location1 = "Seattle"
+location2 = "Iceland"
+location3 = "London"
+
+# Wildcard placeholders can be used to reserve a spot for a variable before explicitly saying what the variable is. Use {} to do so.
+print("I flew from {} to {} to get to {}". format(location1, location2, location3))
