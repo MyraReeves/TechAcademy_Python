2b32e7f5a1b3b776cf369ecd5e421083cb9b6a8c
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-27 23:26:07 -0700

    Shows contents of lesson

---
+++ Part-45-DocString.py
@@ -0,0 +1,8 @@
+# Docstring is an abbreviation of "document string."  It is an alternative way to comment on how a module/function/class works.
+
+def subtractFunction(x, y):
+    """This is a docstring of the subtract function."""
+    return a - b
+
+
+print(subtractFunction.__doc__)
