e3e24ba06fb64c2fc684591705d99cd563da68de
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-27 22:54:25 -0700

    Shows content of lesson

---
+++ Part-42-EscapeSequences.py
@@ -0,0 +1,8 @@
+# To tab indent a line, use \t
+print("\tThis line has been indented.")
+
+# To start a string on a new line use \n
+print("This string is on a line \nbefore this other line")
+
+# To create a carriage return, use \r
+print("\nTHIS LINE WON'T SHOW UP!  \rThis other string is on a new line. \n\tAnd this string was started on a third line, indented with a tab")
