451a6dd0920b1e716ec6e55ac400e241606e7e05
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-28 23:53:00 -0700

    Shows content from the first half of the lecture video

---
+++ Part-49-DunderMethods1.py
@@ -0,0 +1,7 @@
+
+# Dunder (double underline) methods are built in to Python and will perform special actions when used.
+
+def printApp():
+    # Get the name of this file and store it in the variable "name" using the __name__ dunder method:
+    name = (__name__)
+    return name
