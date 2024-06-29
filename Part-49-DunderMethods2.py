f692dfaff97f62228831bf6b37088f22fa91875d
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-29 00:08:25 -0700

    Shows code from the second lecture video for this file

--- Part-49-DunderMethods2.py
+++ Part-49-DunderMethods2.py
@@ -1,9 +1,13 @@
-
 import Part_49_DunderMethods1
 
 def printApp_Part2():
     name = (__name__)
     return name
 
+
 if __name__ == "__main__":
-    print("I am running code from")
+    # Call code from within this script file:
+    print("First, I am running code from {}".format( printApp_Part2() ) )
+
+    # Call code from the imported module:
+    print("And now, instead, I am running code from {}".format( Part_49_DunderMethods1.printApp() ) )
