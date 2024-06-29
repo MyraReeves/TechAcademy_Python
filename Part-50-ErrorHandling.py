c450c6a0c34268cdba898d12a693a8f09ccb1cb3
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-06-29 00:33:43 -0700

    Modifies several lines of code as per the last part of this video lecture

--- Part-50-ErrorHandling.py
+++ Part-50-ErrorHandling.py
@@ -1,26 +1,27 @@
 
 def getInfo():
-    variable1 = input("Please provide the first numeric value: ")
-    variable2 = input("What is the numeric value you wish to add to it? ")
-    compute(variable1, variable2)
+    variable1 = input("\nPlease provide the first numeric value: ")
+    variable2 = input("\nWhat is the numeric value you wish to add to it? ")
+    return(variable1, variable2)
 
-def compute(variable1, variable2):
+def compute():
     go = True
     while go:
-        getInfo()
+        variable1, variable2 = getInfo()
 
         # Add a try/catch block in case of errors
         try:
             variable3 = int(variable1) + int(variable2)
-            print("{} + {} = {}".format(variable1, variable2, variable3))
+            go = False
 
         except ValueError as e:
-            print("{}: \nYou did not provide a numeric value! Make sure you are entering a number".format(e))
+            print("\n{}: \nYou did not provide a numeric value! Make sure you are entering a number".format(e))
+            print("\n\nLet's try again...")
         except:
             print("Invalid input.  The program will close now.")
-        finally:
-            print("Moving on...")
 
+
+    print("{} + {} = {}".format(variable1, variable2, variable3))
 
 if __name__ == "__main__":
-    getInfo()
+    compute()
