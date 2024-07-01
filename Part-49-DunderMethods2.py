0207b296a37d73980e5074050d39d69c9d8de623
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-01 16:55:29 -0700

    Adds explanatory notes

--- Part-49-DunderMethods2.py
+++ Part-49-DunderMethods2.py
@@ -1,13 +1,31 @@
-import Part_49_DunderMethods1
+# "Dunder" is short for "double under(line)".  Double underline methods (also called "magic methods") don't need to be called directly.  Instead, Python calls them when it needs to know how you want something handled - like object initialization. The name comes from the fact that they have two underscore lines on either side, as both a prefix and a suffix.
 
+
+#  The dunder __name__ is a built-in method that is the name of the REMOTE file you are calling on from an outside file, as a string.
+
+
+# Declare a new function that assigns the name of THIS (Part-49-...2) file being typed in to a variable called "name" and then returns it to be seen when the function is called.
 def printApp_Part2():
     name = (__name__)
     return name
+# When called on REMOTELY, this Part2 function will return a value of "Part-49-DunderMethods2.py"
+
+
+
+# Import the other file, the one containing a printApp() function that, when called, will return a variable with a value of "Part_49_DunderMethods1.py"
+import Part_49_DunderMethods1
+
+
+
+# __main__ is a built-in dunder method that is the module script being specifically ran
+
 
 
 if __name__ == "__main__":
-    # Call code from within this script file:
+    # Call the function created above inside this script file:
     print("First, I am running code from {}".format( printApp_Part2() ) )
+    # Since we're calling this function from inside the same file it was created in instead of remotely, instead of showing the name of this file being currently typed in this will show "__main__" because it is the very module being run!
 
-    # Call code from the imported module:
+    # Call code from the imported module file:
     print("And now, instead, I am running code from {}".format( Part_49_DunderMethods1.printApp() ) )
+    # Since this module is saved in a different, remote file, when we call on it the __name__ dunder will show that other file name
--- Part_49_DunderMethods1.py
+++ Part_49_DunderMethods1.py
@@ -1,9 +1,14 @@
-# This file is a module for only saving functions inside of it.  Not calling them.  Call them within a second file.
-
+# This file is a module for only saving functions (block of codes that can be reused) inside of it.  Not for calling them.  Call on them within a second file.  This type of file is called a module.
 
 # Dunder (double underline) methods are built in to Python and will perform special actions when used.
 
+
+
+# Define a function named "printApp" using () and :
 def printApp():
-    # Get the name of this file and store it in the variable "name" using the __name__ dunder method:
+    # Use this function to get the name of this file and store it in a variable called "name" using the __name__ Python dunder method:
     name = (__name__)
+    # Return back the name of this file whenever anyone calls on this printApp function:
     return name
+
+# When called on in a separate file, the above function should return a value of "Part_49_DunderMethods1.py"
