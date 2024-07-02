9d8e295cd7286309f0e61f6f1937dc7f4eb2a8a7
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-01 17:51:35 -0700

    Adds example of an else block

--- Part-51-TryExceptFinallyBlocks.py
+++ Part-51-TryExceptFinallyBlocks.py
@@ -1,5 +1,25 @@
-# Example from w3Schools.com
+# A "try" block lets you test a block of code for errors.  The "except" block lets you handle any errors.
 
+
+
+# An "else" block lets you execute code when no error occurs
+
+# Example in which no error will occur:
+try: print("Hello there!")
+except: print("Something went wrong causing you to lose this light saber battle")
+else: print("I am Obi-wan Kenobi and I have the high ground!")
+
+
+
+
+print("-------------------------")
+
+
+
+
+# A "finally" block lets you execute code, regardless of success/failure results of a try/except block.
+
+# Example from w3Schools.com:
 try:
     f = open("demofile.txt")
     try:
@@ -12,6 +32,6 @@ try:
         print("Closing program...")
         f.close()
 
-# This block will run if an error is thrown when attempting to open the file:
+# This block will run if an error is thrown when attempting to open the file, which is what will happen since there is no "demofile.txt" file here:
 except:
     print("Something went wrong when opening the file")
