333a854a95f9fc5d9a10b80f3f93cde451a69108
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-03 00:48:00 -0700

    Shows content of lecture video

--- Part-52-File_IO.py
+++ Part-52-File_IO.py
@@ -8,6 +8,52 @@ print(help(str))
 
 
 
+
+
 # ###################################
 #           OS Module               #
 # ###################################
+
+import os
+
+print(dir(os))
+
+print(help(os))
+
+
+# To return a unicode string showing the current working directory, use getcwd()
+print(os.getcwd())
+
+
+print(help(open))
+# To open a file to read-only it use open() within a with loop, creating a named instance
+# In the example below, the instance is called f, and we created a new variable called "data" that read the contents of the file.  The contents were stored inside of the "data" variable.  Hence, we printed the "data" variable to be able to view it.  Then to prevent any memory leaks, we close the file.
+with open('C:\Users\4myra\OneDrive\Desktop\Projects\DevelopmentArea\python_projects\SchoolExample.txt', 'r') as f:
+    data = f.read()
+    print(data)
+    f.close()
+
+
+# To add content to a file, use "append" instead of "write", because "w" would overwrite/truncate the existing file!
+data = '\nHello there!'
+with open('README.md', 'a') as f:
+    f.write(data)
+    f.close()
+
+
+# Those commands can be saved within functions:
+def openFile():
+    with open('README.md', 'r') as f:
+        data = f.read()
+        print(data)
+        f.close()
+
+def writeData():
+    data = '\nHello there!'
+    with open('README.md', 'a') as f:
+        f.write(data)
+        f.close()
+
+if __name__ == "__main__":
+    writeData()
+    openFile()
