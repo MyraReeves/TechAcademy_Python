7d74960d1137f4dce6c2742131f5270431c8083f
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-02 23:59:18 -0700

    Explains some basic commands for understanding methods in Python

---
+++ Part-52-File_IO.py
@@ -0,0 +1,13 @@
+
+# To see all of the methods associated with the string class:
+print(dir(str))
+
+# To open up the built-in help module associated with strings, to see the correct syntax and an explanation of what a method does:
+print(help(str))
+
+
+
+
+# ###################################
+#           OS Module               #
+# ###################################
