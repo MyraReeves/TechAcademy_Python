1556334aeca5bcbff5591f576a083a69ed6b071f
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-06 00:59:23 -0700

    Note: Final step is still unsolved

--- Part-54-ScriptAssignment.py
+++ Part-54-ScriptAssignment.py
@@ -15,18 +15,21 @@ directoryContents = os.listdir(desiredDirectory)
 
 
 # Viewing that list of all the files to ensure that worked:
-print("All the files currently inside of the python_projects directory are:")
+print("\n All the files currently inside of the python_projects directory are:")
 print(directoryContents)
 
 
 # Setting up the console message for the next step:
-print("\nIn specific, all of the text files in the directory are:")
+print("\nIn specific, the absolute file paths to all of the TEXT files in the directory are:")
+
+fileName = []
 
 # To narrow it down to only .txt files use a For loop:
 textDocuments = []
 
 for file in os.listdir(desiredDirectory):
     if file.endswith(".txt"):
+        fileName += [file]
         textDocuments += [os.path.join(desiredDirectory, file)]
         # Use the path.join() method from the OS module to concatenate the file name to its file path, forming an absolute path:
         print(os.path.join(desiredDirectory, file))
@@ -34,9 +37,19 @@ for file in os.listdir(desiredDirectory):
 
 # Use the getmtime() method from the OS module to find the latest date that each text file was created or modified:
 import time
+
 modification_time = os.path.getmtime(textDocuments[0])
 # convert the time in seconds since epoch into local time
 local_time = time.ctime(modification_time)
-print("\nLast modification time (Local time):", local_time)
+print("\nThe last modification (in your local time) of", fileName[0], "occured at", local_time)
 
-# Print to the console a list of each file ending with a ".txt" file extension, and its corresponding mtime:
+# Shortening it to one line:
+local_time2 = time.ctime(os.path.getmtime(textDocuments[1]))
+print("\nThe last modification of", fileName[1], "occured at", local_time2, "\n")
+
+# To print to the console a list of each file ending with a ".txt" file extension and its corresponding mtime, use a For loop:
+timeStamp = []
+# for modifiedTime in os.listdir(desiredDirectory):
+#     timeStamp += [time.ctime(os.path.getmtime(directoryContents))]
+
+for x in fileName: print(x, timeStamp)
