57f794dd8471bae91e367b03eccca8deff5b9a29
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-07 00:04:32 -0700

    Shows lesson content

--- Part-57-SQL&Tuples.py
+++ Part-57-SQL&Tuples.py
@@ -1,6 +1,6 @@
 
 import sqlite3
-connection = sqlite3.connect(test.db)
+connection = sqlite3.connect('test.db')
 with connection:
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS more_people(ID INTEGER PRIMARY KEY AUTOINCREMENT,\
@@ -15,4 +15,14 @@ persons_tuple = ('Kelly', 'Sally', 'Kevin', 'Adam')
 # To add only the names that end with "y" to the database...
 # Create a For loop that analyzes the tuple of names and finds only those that end with "y", then splits the names we want into one-element tuples:
 
-for x
+# Loop thru each object in the tuple to find the names that end in the letter "y":
+for x in persons_tuple:
+    if x.endswith('y'):
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO more_people (fName) VALUES (?)", (x,))
+            print(x)
+        # The values for each row will be one name out of the tuple.  Therefore (x,) will denote a one element tuple for each name ending with y.
+
+connection.close()
+
