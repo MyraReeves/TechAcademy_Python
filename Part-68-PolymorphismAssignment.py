dbe0d3c982c55722d8cdc3ea81815857527c637d
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-07 23:57:05 -0700

    Fulfills assignment

--- Part-68-PolymorphismAssignment.py
+++ Part-68-PolymorphismAssignment.py
@@ -1,12 +1,51 @@
+print("\n")
 # Assignment = create two classes that inherit from another class
 
 # The parent should have at least one method function
+class Vehicle:
+    def __init__(self, brandName, modelName):
+        self.brand = brandName
+        self.model = modelName
 
-# Each child should have at least two of their own attributes.
+    def movement(self):
+        print("Race!")
 
-# Both child classes should utilize polymorphism on the parent class method
+# Each child should have at least two of their own attributes. The child classes should utilize polymorphism on the parent class method.
+class Sportscar(Vehicle):
+    pass
+
+
+class Speedboat(Vehicle):
+    Year = " "
+    TelevisionShow = " "
+    def movement(self):
+        print("Accelerate across the water!")
+
+
+class Fighterjet(Vehicle):
+    Flown_by = "Unknown"
+    MovieTitle = " "
+    def movement(self):
+        print("Negative, ghostrider. The pattern is full.")
+
+
+# A sportcar object:
+Richard_Petty = Sportscar("Plymouth", "Superbird")
+# A speedboat object:
+Undercover = Speedboat("Wellcraft", "Scarab")
+# A fighter jet object:
+Maverick = Fighterjet("F-14", "Tomcat")
+
+
+# Using a For loop to call on each of the objects:
+for x in (Richard_Petty, Undercover, Maverick):
+    print(x.brand, x.model)
+    x.movement()
+
 
 # ____________________________________________________________________________________________________________________
+print("\n")
+
 
 # Example of polymorphism WITHOUT inheritance:
 class Car:
@@ -40,6 +79,6 @@ MiamiVice = Boat("Ibiza", "Touring 20")
 # Creation of a Plane object:
 Decommissioned = Airplane("Boeing", "747")
 
-# Calling on the move method they all have:
+# Uses a For loop to call on the "move" method they all have:
 for x in (Speedracer, MiamiVice, Decommissioned):
     x.move()
