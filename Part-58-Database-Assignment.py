071776208ab07a8f47493287b3e1ce5b3893b796
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-09 23:44:35 -0700

    Adds assignment instructions

---
+++ Part-58-Database-Assignment.py
@@ -0,0 +1,12 @@
+# Using Python and the SQLite3 module, write a script that creates a database and adds new data into that database
+
+# Requirements = 
+# Include in the database 2 fields - an autoincrement primary integer field and a "string" field
+# Read from the supplied list of files names and determine only the names that end in a "txt" file extension
+# The provided folder and files do NOT need to be saved on your computer
+# The script should add the found txt file names to the database
+# The list of qualifying text file names should be printed to the console
+
+# Provided file name list:
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', 'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
