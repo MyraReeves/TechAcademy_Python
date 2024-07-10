f947cbcfd3728af4c7992e6f5f32cc2c2424a77d
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-09 23:58:03 -0700

    Working on adding needed codes

--- Part-58-Database-Assignment.py
+++ Part-58-Database-Assignment.py
@@ -6,7 +6,39 @@
 # The provided folder and files do NOT need to be saved on your computer
 # The script should add the found txt file names to the database
 # The list of qualifying text file names should be printed to the console
+# --------------------------------------------------------------------------------------------------
 
-# Provided file name list:
+# Connecting to sqlite:
+import sqlite3
+
+
+
+with connection:
+    # Cursor object:
+    cursor = connection.cursor()
+
+    # Create a "text file name" table, if one doesn't already exist:
+    cursor.execute("CREATE TABLE IF NOT EXISTS text_file_name(ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+                   fileName TEXT)")
+    
+    connection.commit()
+
+
+
+
+# Provided tuple of file names:
 fileList = ('information.docx', 'Hello.txt', 'myImage.png', 'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
 
+# Creating a For loop that analyzes that tuple of file names and finds only those that end with ".txt", then splits those names we want into being one-element tuples:
+for x in fileList:
+    if x.endswith('.txt'):
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO more_people (fName) VALUES (?)", (x,))
+            print(x)
+        # The values for each row will be one name out of the tuple.  Therefore (x,) will denote a one element tuple for each name ending with y.
+
+
+
+# Closing the connection:
+connection.close()
