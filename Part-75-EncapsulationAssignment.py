8a1d269f35fa483e3c3666a5865def932587c5e2
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-26 23:40:39 -0700

    Adds a possible solution to requirement #1

--- Part-75-EncapsulationAssignment.py
+++ Part-75-EncapsulationAssignment.py
@@ -1,7 +1,21 @@
 # Create a class that uses encapsulation. Requirements include:
 
+# 1. This class should make use of a private attribute or function:
 
-# This class should make use of a private attribute or function.
+# Creating a class for student
+class Student:
+    name = "April Glass"
+    __id = 1234567
+
+    # Method for printing the private attribute:
+    def printId(self):
+        print(f"The id # of this student is: {self.__id}")
+
+Glass = Student()
+print(f"The name of this student is: {Glass.name}")
+
+# Accessing the private attribute using getter method:
+Glass.printId()
 
 # This class should make use of a protected attribute or function.
 
