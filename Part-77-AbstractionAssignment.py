a0eef978a1394d77b3bbb3d3eaa0f6023a754572
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-26 22:35:32 -0700

    Adds assignment requirements

---
+++ Part-77-AbstractionAssignment.py
@@ -0,0 +1,15 @@
+# ABSTRACTION ASSIGNMENT
+
+# Create a class that utilizes the concept of abstraction.
+
+
+# Your class should contain at least one abstract method and one regular method.  
+
+# Create a child class that defines the implementation of its parents abstract method.
+
+# Create an object that utilizes both the parent and child methods.
+ 
+
+# Add comments throughout your Python explaining your code.
+
+# Upload your code to GitHub.
