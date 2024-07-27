f0423a73a96ef933319e611d1add84f41d3ba91c
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-26 23:58:16 -0700

    New example of answer. Sets up ATM class with functions

--- Part-75-EncapsulationAssignment.py
+++ Part-75-EncapsulationAssignment.py
@@ -1,19 +1,36 @@
 # Create a class that uses encapsulation. Requirements include:
 
-# 1. This class should make use of a private attribute or function:
+# 1. This class should make use of a private attribute or function.
+# 2. This class should make use of a protected attribute or function.
 
-# Creating a class:
-class MyClass:
-    def _private_method(self):
-        print("This is my private method")
+class BankAccount:
+    def __init__(self, account_holder, intitial_balance=0):
+        self.account_holder = account_holder
 
-# Creating an object from that class:
-object1 = MyClass()
+        # Private attribute:
+        self.__balance = intitial_balance
 
-# This class should make use of a protected attribute or function.
 
-# Create an object that makes use of protected and private.
+    def depositMoney(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"Successfully deposited ${amount} into your account. \nNew account balance: ${self.__balance}")
+        else:
+            print("ERROR: Invalid amount!  Can not deposit negative dollars!")
 
-# Add comments throughout your Python explaining your code.
 
-# Upload your code to GitHub.
+    def withdrawMoney(self, amount):
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+            print(f"Please collect your withdrawl of ${amount} cash below. \nNew account balance: ${self.__balance}")
+        else:
+            print("ERROR: Insufficient funds!")
+
+    
+    # Private method:
+    def __update_balance(self, amount):
+        self.__balance += amount
+
+
+
+# 3. Create an object that makes use of protected and private.
