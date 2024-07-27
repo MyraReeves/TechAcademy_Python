3d6cd0fc6f0e6a00bef31353f35ba17086352eff
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-26 22:33:23 -0700

    Adds assignment requirements

---
+++ Part-75-EncapsulationAssignment.py
@@ -0,0 +1,12 @@
+# Create a class that uses encapsulation. Requirements include:
+
+
+# This class should make use of a private attribute or function.
+
+# This class should make use of a protected attribute or function.
+
+# Create an object that makes use of protected and private.
+
+# Add comments throughout your Python explaining your code.
+
+# Upload your code to GitHub.
