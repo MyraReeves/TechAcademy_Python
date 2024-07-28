321c125400c1eb370fae063e8eecb5906d12c0ea
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-07-27 22:24:44 -0700

    Fulfills assignment

--- Part-77-AbstractionAssignment.py
+++ Part-77-AbstractionAssignment.py
@@ -1,15 +1,33 @@
 # ABSTRACTION ASSIGNMENT
 
-# Create a class that utilizes the concept of abstraction.
+from abc import ABC, abstractmethod
 
+# Create a class that utilizes the concept of abstraction.  Your class should contain at least one abstract method and one regular method.
+class Animal(ABC):
+    def identifying(self):
+        print("\nI am an animal....")
 
-# Your class should contain at least one abstract method and one regular method.  
+    def movement(self):
+        pass
+
 
 # Create a child class that defines the implementation of its parents abstract method.
+class Dog(Animal):
+    def movement(self):
+        print("...who can walk and run.")
+
+
+class Snake(Animal):
+    def movement(self):
+        print("...and I can only crawl on my belly!")
+
+
 
 # Create an object that utilizes both the parent and child methods.
- 
+Object1 = Snake()
+Object1.identifying()
+Object1.movement()
 
-# Add comments throughout your Python explaining your code.
-
-# Upload your code to GitHub.
+Object2 = Dog()
+Object2.identifying()
+Object2.movement()
