a7610564e88240fb06d24617d8f8a2834e90530e
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-01 01:17:57 -0700

    Adds details of assignment

---
+++ Part-80-DatabaseChallenge.py
@@ -0,0 +1,19 @@
+# STEP 305 = Complete these actions:
+
+# 1. Create a database table in RAM named Roster that includes the fields ‘Name’, ‘Species’ and ‘IQ.’
+
+
+# 2. Populate your new table with the following values:
+
+# 1 Jean-Baptiste Zorg, Human, 122
+# 2 Korben Dallas, Meat Popsicle, 100
+# 3 Ak'not, Mangalore, -5
+
+
+# 3. Update the Species of Korben Dallas to be Human.
+
+
+# 4. Display the names and IQs of everyone in the table who is classified as Human.
+
+
+# HELPFUL LINK:  https://www.sqlitetutorial.net/sqlite-python/creating-database/
