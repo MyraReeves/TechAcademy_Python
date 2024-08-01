c6839787706c308558a838e37e53dd93a8688d54
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-01 02:07:51 -0700

    Adds assignment questions/challenge to answer

---
+++ Part-83-DatetimeChallenge.py
@@ -0,0 +1,13 @@
+# STEP 308 = DATETIME CHALLENGE
+
+# The Portland-based company you work for just opened two new branches. One is in New York City, the other in London. They need a very simple program to find out if the branches are open or closed. The hours of both branches are 9:00 a.m.-5:00 p.m. in their own time zone.
+
+# Using IDLE, create a new file.
+
+# Import the datetime module and any others to aid in time zone calculations.
+
+# Create a script that will find out the current times in the Portland HQ and NYC and London branches. Then, compare that time with each branch's hours to see if they are open or closed.
+
+# Print out to the screen the three branches and whether they are open or closed.
+
+# HELPFUL LINK:   https://www.w3schools.com/python/python_datetime.asp
