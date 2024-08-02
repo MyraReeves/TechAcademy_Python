8793502fe027bd5c88e6789a99d85b0cf82d1521
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-02 00:41:31 -0700

    Adds functionality to Source and Destination buttons

--- Homework--file_transfer.py
+++ Homework--file_transfer.py
@@ -1,26 +1,51 @@
 import tkinter as tk
 from tkinter import *
+import tkinter.filedialog
 
 class ParentWindow (Frame):
     def __init__(self, master):
         Frame.__init__(self)
-        self.master.title("Move Files")
+        self.master.title("Transfer All Files To New Folder")
 
         # "Select Source" Button:
-        self.sourceDirectoryButton = Button(text="Select Source", width=20, bg="papayawhip")
+        self.sourceDirectoryButton = Button(text="Source Folder", width=20, bg="papayawhip", command=self.sourceDir)
         self.sourceDirectoryButton.grid(row=0, column=0, padx=(20, 10), pady=(30,0))
 
         # Entry area for the source:
-        self.sourceDirectory = Entry(width=75, bg="papayawhip")
-        self.sourceDirectory.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))   # padx and pady are the same as the button to ensure they will line up.
+        self.sourceDirectoryEntry = Entry(width=75, bg="papayawhip")
+        self.sourceDirectoryEntry.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))   # padx and pady are the same as the button to ensure they will line up.
 
         # "Select Destination" Button:
-        self.destinationDirectoryButton = Button(text="Select Destination", width=20, bg="lavender")
+        self.destinationDirectoryButton = Button(text="Destination Folder", width=20, bg="lavender", command=self.destiDir)
         self.destinationDirectoryButton.grid(row=1, column=0, padx=(20, 10), pady=(15, 10))
 
         # Entry area for the destination:
-        self.destinationDirectory = Entry(width=75, bg="lavender")
-        self.destinationDirectory.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10)) # padx and pady are the same as the button to ensure they will line up.
+        self.destinationDirectoryEntry = Entry(width=75, bg="lavender")
+        self.destinationDirectoryEntry.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10)) # padx and pady are the same as the button to ensure they will line up.
+
+
+
+
+    # Function to select the source directory:
+    def sourceDir(self):
+        selectSourceDirectory = tkinter.filedialog.askdirectory()
+
+        # Clear contents of the source Entry widget (from index zero thru the end) to allow the newly selected path to be inserted:
+        self.sourceDirectoryEntry.delete(0, END)
+
+        # Insert the file path of the folder that the user clicks on into the source Entry widget:
+        self.sourceDirectoryEntry.insert(0, selectSourceDirectory)
+
+
+    # Function to select the destination directory:
+    def destiDir(self):
+        selectDestinationDirectory = tkinter.filedialog.askdirectory()
+
+        # Clear contents of destination Entry widget to allow the newly selected file path to be inserted:
+        self.destinationDirectoryEntry.delete(0, END)
+
+        # Insert into the destination Entry widget the file path of the folder the user clicks on:
+        self.destinationDirectoryEntry.insert(0, selectDestinationDirectory)
 
 
 
