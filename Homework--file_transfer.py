81884086922419251bbc5c044a618367a902535f
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-02 00:11:28 -0700

    Adds background color to selection and destination areas

--- Homework--file_transfer.py
+++ Homework--file_transfer.py
@@ -7,19 +7,19 @@ class ParentWindow (Frame):
         self.master.title("Move Files")
 
         # "Select Source" Button:
-        self.sourceDirectoryButton = Button(text="Select Source", width=20)
+        self.sourceDirectoryButton = Button(text="Select Source", width=20, bg="papayawhip")
         self.sourceDirectoryButton.grid(row=0, column=0, padx=(20, 10), pady=(30,0))
 
         # Entry area for the source:
-        self.sourceDirectory = Entry(width=75)
+        self.sourceDirectory = Entry(width=75, bg="papayawhip")
         self.sourceDirectory.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))   # padx and pady are the same as the button to ensure they will line up.
 
         # "Select Destination" Button:
-        self.destinationDirectoryButton = Button(text="Select Destination", width=20)
+        self.destinationDirectoryButton = Button(text="Select Destination", width=20, bg="lavender")
         self.destinationDirectoryButton.grid(row=1, column=0, padx=(20, 10), pady=(15, 10))
 
         # Entry area for the destination:
-        self.destinationDirectory = Entry(width=75)
+        self.destinationDirectory = Entry(width=75, bg="lavender")
         self.destinationDirectory.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10)) # padx and pady are the same as the button to ensure they will line up.
 
 
