c26d237f28ceedc1e7433281edfcb4210182015e
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-03 19:24:07 -0700

    Adds functional Exit button to GUI

--- Homework--file_transfer.py
+++ Homework--file_transfer.py
@@ -16,7 +16,7 @@ class ParentWindow (Frame):
 
         # Display area for the chosen source:
         self.sourceDirectoryEntry = Entry(width=75, bg="papayawhip")
-        self.sourceDirectoryEntry.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))   # padx and pady are the same as the button to ensure they will line up.
+        self.sourceDirectoryEntry.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))          # pady is the same as the button to ensure they will line up, and padx is the same as File Transfer button to line up their left edges.
 
         # "Select Destination" button:
         self.destinationDirectoryButton = Button(text="Destination Folder", width=20, bg="lavender", command=self.destiDir)
@@ -24,11 +24,15 @@ class ParentWindow (Frame):
 
         # Display area for the chosen destination:
         self.destinationDirectoryEntry = Entry(width=75, bg="lavender")
-        self.destinationDirectoryEntry.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10)) # padx and pady are the same as the button to ensure they will line up.
+        self.destinationDirectoryEntry.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10))        # pady is the same as the button to ensure they will line up, and padx is the same as File Transfer button to line up their left edges.
 
         # File Transfer button:
-        self.transferButton = Button(text="Transfer All Files", width = 30, fg="darkgreen", bg="palegreen", command=self.transferFiles)
-        self.transferButton.grid(row=2, column=0, padx=(200, 0), pady=(0, 15))
+        self.transferButton = Button(text="Transfer All Files", width = 20, height=2, bg="palegreen", command=self.transferFiles)
+        self.transferButton.grid(row=2, column=1, padx=(20, 0), pady=(10, 25), sticky=W)        # padx is the same as the Entry display areas to ensure its left edge lines up vertically underneath
+
+        # Exit button:
+        self.exitButton = Button(text="Exit Program", width=13, height=2, fg="darkred", bg="pink", command=self.exitProgram)
+        self.exitButton.grid(row=2, column=2, padx=(0, 10), pady=(10, 25), sticky=E)
 
 
 
@@ -67,6 +71,14 @@ class ParentWindow (Frame):
             shutil.move(source + '/' + iteration, destination)
             # Confirm file transfer in console:
             print('\n ✔️ ' + iteration + ' successfully transferred to \n' + destination)
+
+
+    # Function to exit program:
+    def exitProgram(self):
+        # Terminate the main GUI window and all windows inside of it
+        root.destroy()
+
+
 
 
 if __name__ == "__main__":
