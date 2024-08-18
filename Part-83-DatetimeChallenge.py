6254d7646c371d08777216bee4a30b039e8ee482
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-17 20:30:53 -0700

    Partial completion of assignment

--- Part-83-DatetimeChallenge.py
+++ Part-83-DatetimeChallenge.py
@@ -5,9 +5,27 @@
 # Using IDLE, create a new file.
 
 # Import the datetime module and any others to aid in time zone calculations.
+import datetime
+from zoneinfo import ZoneInfo
 
 # Create a script that will find out the current times in the Portland HQ and NYC and London branches. Then, compare that time with each branch's hours to see if they are open or closed.
+PortlandHQ = datetime.datetime.now()
+PortlandHour_Minute = (PortlandHQ.strftime("%H:%M:%S on %B %d"))
+
+EasternTime = ZoneInfo('America/New_York')
+NewYorkCity = PortlandHour_Minute.astimezone(EasternTime)
+
+# London = PortlandHQ +8
+
 
 # Print out to the screen the three branches and whether they are open or closed.
+print("\nThe time at headquarters in Portland, Oregon, is currently: ", PortlandHour_Minute)
+print("Portland HQ is ")
+
+print("\nThe time in New York City is currently: ", NewYorkCity)
+# print("The branch is NYC is ")
+
+# print("\nThe current time in London is: ", London)
+# print("The London branch is ")
 
 # HELPFUL LINK:   https://www.w3schools.com/python/python_datetime.asp
