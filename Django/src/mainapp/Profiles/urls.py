8a256be679700660560ab3622e8240a3ddd087cd
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-19 04:17:21 -0700

    Adds path and nickname for Profile details page

--- Django/src/mainapp/Profiles/urls.py
+++ Django/src/mainapp/Profiles/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('users', views.username_list, name="users"),
+    path('users', views.username_list, name="users"),
+
+    # path(full url path, call the "details" method from views file, nickname of this path)
+    path('<int:pk>/details', views.details, name="details"),
 ]
