b37ef21c404ccb33a3dc69e7d293673bc774aa6c
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-19 04:27:42 -0700

    Adds missing backslash in url

--- Django/src/mainapp/Profiles/urls.py
+++ Django/src/mainapp/Profiles/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('users', views.username_list, name="users"),
 
     # path(full url path, call the "details" method from views file, nickname of this path)
-    path('<int:pk>/details', views.details, name="details"),
+    path('<int:pk>/details/', views.details, name="details"),
 ]
