a0f98a039daf13b60d716222413087983a27c4be
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-20 17:08:27 -0700

    Adds path to delete webpage

--- Django/src/mainapp/Profiles/urls.py
+++ Django/src/mainapp/Profiles/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users', views.username_list, name="users"),
-
     # path(full url path, call the "details" method from views file, nickname of this path)
+    path('users', views.username_list, name="users"),
     path('<int:pk>/details/', views.details, name="details"),
+    path('<int:pk>/delete/', views.delete, name="delete"),
 ]
