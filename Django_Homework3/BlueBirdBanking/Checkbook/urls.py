ddaa644b8ddc20d663c6c429aa98a103e1722856
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-21 23:57:42 -0700

    Adds url paths for homepage, CreateNewAccount, BalanceSheet, and AddNewTransaction pages

---
+++ Django_Homework3/BlueBirdBanking/Checkbook/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='index'),
+    path('create_new_account', views.create_account, name='create'),
+    path('balance_sheet', views.balance, name='balance'),
+    path('add_new_transaction', views.transaction, name='transaction'),
+]
