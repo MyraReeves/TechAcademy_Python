a37db2f4bb9ec64c7a87c969e96fe945a2f296ce
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-22 21:48:23 -0700

    Fixes digits syntax error and adds Transaction model

--- Django_Homework3/BlueBirdBanking/Checkbook/models.py
+++ Django_Homework3/BlueBirdBanking/Checkbook/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Account(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    initial_deposit = models.DecimalField(max_length=15, decimal_places=2)
+    initial_deposit = models.DecimalField(max_digits=15, decimal_places=2)
 
     # Model manager:
     object = models.Manager()
@@ -13,3 +13,17 @@ class Account(models.Model):
     # Refers accounts as a string of the owner's full name:
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+
+TransactionTypes = [('Deposit', 'Deposit'), ('Withdrawal', 'Withdrawal'), ('Debit', 'Debit')]
+
+
+class Transaction(models.Model):
+    date = models.DateField()
+    type = models.CharField(max_length=15, choices=TransactionTypes)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.CharField(max_length=100)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    # Model manager
+    object = models.Manager()
