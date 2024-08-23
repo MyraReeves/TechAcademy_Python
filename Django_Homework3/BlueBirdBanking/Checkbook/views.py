9eac736ef834c3af649fa13762ff74bc29a887c6
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-22 22:49:17 -0700

    Modifies balance function to pull all the transactions associated with an account (via primary key) to calculate the current account balance

--- Django_Homework3/BlueBirdBanking/Checkbook/views.py
+++ Django_Homework3/BlueBirdBanking/Checkbook/views.py
@@ -29,8 +29,33 @@ def create_account(request):
         return render(request, 'checkbook/CreateNewAccount.html', content)
 
 
-def balance(request):
-    return render(request, 'checkbook/BalanceSheet.html')
+def balance(request, pk):
+    # Retrieve the requested account using its primary key:
+    account = get_object_or_404(Account, pk=pk)
+
+    # Retrieve all of that account's transactions:
+    transactions = Transaction.object.filter(account=pk)
+
+    # Start the value of a "current total" variable as being the value of the initial deposit reported
+    current_total = account.initial_deposit
+
+    # Create a dictionary to place transaction information into:
+    table_contents = {}
+
+    for t in transactions:
+        # For deposits, add the amount to the current total and then add the transaction to the dictionary:
+        if t.type == 'Deposit':
+            current_total += t.amount
+            table_contents.update({t: current_total})
+
+        # For withdrawals, subtract the amount from the current total and then add the transaction to the dictionary:
+        else:
+            current_total -= t.amount
+            table_contents.update({t: current_total})
+
+    # Pass the account, transaction dictionary, and current total balance to the template, and then return the info:
+    content = {'account': account, 'table_contents': table_contents, 'balance': current_total}
+    return render(request, 'checkbook/BalanceSheet.html', content)
 
 
 def transaction(request):
