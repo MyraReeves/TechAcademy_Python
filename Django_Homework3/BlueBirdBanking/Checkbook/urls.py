4caddde3fd95e532ff4a8d3be253bc943f1e2081
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-22 20:53:41 -0700

    Adds slash at end of url

--- Django_Homework3/BlueBirdBanking/Checkbook/urls.py
+++ Django_Homework3/BlueBirdBanking/Checkbook/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('create_new_account', views.create_account, name='create'),
-    path('balance_sheet', views.balance, name='balance'),
-    path('add_new_transaction', views.transaction, name='transaction'),
+    path('create_new_account/', views.create_account, name='create'),
+    path('balance_sheet/', views.balance, name='balance'),
+    path('add_new_transaction/', views.transaction, name='transaction'),
 ]
