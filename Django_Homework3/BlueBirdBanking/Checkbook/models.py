7a56f95b78746675a24287343f9381b764a1e1ae
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-22 21:38:28 -0700

    Adds an Account model

--- Django_Homework3/BlueBirdBanking/Checkbook/models.py
+++ Django_Homework3/BlueBirdBanking/Checkbook/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Account(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    initial_deposit = models.DecimalField(max_length=15, decimal_places=2)
+
+    # Model manager:
+    object = models.Manager()
+
+    # Refers accounts as a string of the owner's full name:
+    def __str__(self):
+        return self.first_name + " " + self.last_name
