f1670640e31020074bcf918a7037f34a8fa05d20
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-08-22 22:21:15 -0700

    Modifies balance url so that the balance shet can be displayed using a referenced primary key

--- Django_Homework3/BlueBirdBanking/Checkbook/urls.py
+++ Django_Homework3/BlueBirdBanking/Checkbook/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='index'),
     path('create_new_account/', views.create_account, name='create'),
-    path('balance_sheet/', views.balance, name='balance'),
+    path('<int:pk>/balance_sheet/', views.balance, name='balance'),
     path('add_new_transaction/', views.transaction, name='transaction'),
 ]
